6e0fd6c0640e0ef03e050db3627d24adb9f7244e
Adam Aslan <chillcoders@gmail.com>
2021-09-28 17:54:12 -0400

    auth2-dependz

--- pages/posts/login.js
+++ pages/posts/login.js
@@ -1,1 +1,16 @@
-//nuttin
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="YOUR_DOMAIN"
+    clientId="YOUR_CLIENT_ID"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
