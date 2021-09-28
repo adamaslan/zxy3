888d1faaa62959300cf24bf3eacab36718758fbe
Adam Aslan <chillcoders@gmail.com>
2021-09-28 18:32:57 -0400

    login

--- pages/posts/login.js
+++ pages/posts/login.js
@@ -1,16 +1,20 @@
 
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
+import React from 'react';
 
-ReactDOM.render(
-  <Auth0Provider
-    domain="YOUR_DOMAIN"
-    clientId="YOUR_CLIENT_ID"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById("root")
-);
+export default function Login() {
+  return(
+    <form>
+      <label>
+        <p>Username</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Password</p>
+        <input type="password" />
+      </label>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  )
+}
