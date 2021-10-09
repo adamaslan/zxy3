57c75cf5b380b62fccaff1c8eb6b707e1a517710
Adam Aslan <chillcoders@gmail.com>
2021-10-08 21:40:53 -0400

    Update about.js

--- pages/posts/about.js
+++ pages/posts/about.js
@@ -9,7 +9,7 @@ export default function About() {
         <Head>
           <title>ZXY</title>
         </Head>
-        <h1>About Zxy Gallery</h1><br /> <h2> We are always looking for artists of various sorts! Contact us today! </h2>  <h2> ZXY Gallery looks to bring together artists from a variety of backgrounds. Inclusivity and safety are primary concerns. We are proud to show work from the QTBIPOC, immigrants, women, and other communities that have not traditionally had the same opportunities in the art world.<br /> We also believe that much of the "progress" that has been made is a band-aid on a gaping wound. One thing that sets us apart from other galleries is our ability to create a safe space for dialogue. As such, we have had many artists discuss their work at the gallery.</h2><br /><br /> 
+        <h1>About Zxy Gallery</h1><br /> <h2> We are always looking for artists, especially artists that can show work outdoors. Contact us on instagram @zxygallery. </h2>  <h2> ZXY Gallery looks to bring together artists from a variety of backgrounds. Inclusivity and safety are primary concerns. We are proud to show work from the QTBIPOC, immigrants, women, and other communities that have not traditionally had the same opportunities in the art world.<br /> We also believe that much of the "progress" that has been made is a band-aid on a gaping wound. One thing that sets us apart from other galleries is our ability to create a safe space for dialogue. As such, we have had many artists discuss their work at the gallery.</h2><br /><br /> 
         <h2>How it was built: <br /> This website is built with Nextjs, React, CSS, HTML. It is super fast via its integration with vercel, which also allows for testing before deployment. It runs via github.
 </h2>
 
