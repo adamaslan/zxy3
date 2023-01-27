8cea213d06f669a57c0c523765fa411af3c90dc5
Adam <chillcoders@gmail.com>
2023-01-26 22:30:12 -0500

    revert

--- pages/posts/currentshow.js
+++ pages/posts/currentshow.js
@@ -11,27 +11,40 @@ export default function PastShows() {
           <title>ZXY</title>
         </Head>
         <article>
-        <h1>Current Show</h1>
-        <h2>"Invoking the Amphibian"</h2>
-    
- <Image src="/frogshow.jpeg" alt="Flyer" width={500} height={700} />
-    
-    <p>
-         To build upon the shaky foundations of the current epoch, we must be hybrids. Survival depends on both physical and mental security, which can be at risk at any time. Amphibians provide a model of adaptability. Invoking the amphibian seems like a safe bet for survival. We must both adapt to the present conditions given to us and also evolve for a future uniquely to  provide ourselves the skills to allow us to flourish in a new paradigm. Flash floods ravage NYC and forest fires burn through Turkey. The air may not be safe one day and water the next. Amphibious life could become the norm. While lessons on survival can be learned from amphibians, French sociologist and anthropologist Bruno Latour provides the concept of hybrids that focuses on entities existing both in a natural and social realm. Attempts to divorce one from the other, paradoxically just reinforces their connection. In this sense, to survive, we all must be like frogs. 
+          <h1>Current Show</h1>
+          <h2>"Invoking the Amphibian"</h2>
 
-        </p>
+          <Image src="/frogshow.jpeg" alt="Flyer" width={500} height={700} />
 
-        <h2>
           <p>
-            Find more on our instagram{" "}
-            <a href="https://www.instagram.com/zxygallery/">
-              <a>@zxygallery </a>
-            </a>{" "}
+            To build upon the shaky foundations of the current epoch, we must be
+            hybrids. Survival depends on both physical and mental security,
+            which can be at risk at any time. Amphibians provide a model of
+            adaptability. Invoking the amphibian seems like a safe bet for
+            survival. We must both adapt to the present conditions given to us
+            and also evolve for a future uniquely to provide ourselves the
+            skills to allow us to flourish in a new paradigm. Flash floods
+            ravage NYC and forest fires burn through Turkey. The air may not be
+            safe one day and water the next. Amphibious life could become the
+            norm. While lessons on survival can be learned from amphibians,
+            French sociologist and anthropologist Bruno Latour provides the
+            concept of hybrids that focuses on entities existing both in a
+            natural and social realm. Attempts to divorce one from the other,
+            paradoxically just reinforces their connection. In this sense, to
+            survive, we all must be like frogs.
           </p>
-          <Link href="/">
-            <a>Back to home</a>
-          </Link>
-        </h2>
+
+          <h2>
+            <p>
+              Find more on our instagram{" "}
+              <a href="https://www.instagram.com/zxygallery/">
+                <a>@zxygallery </a>
+              </a>{" "}
+            </p>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </h2>
         </article>
       </Layout>
     </>
