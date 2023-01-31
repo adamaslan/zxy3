cab65bbf2882d3576172fedfa12a901ac9874ff9
Adam <chillcoders@gmail.com>
2023-01-31 18:22:16 -0500

    revert-conflicts

--- pages/posts/pastshows.js
+++ pages/posts/pastshows.js
@@ -1,9 +1,24 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
-import Searchy2 from "../../components/Search2";
+import { getAllUsers } from "../../components/Search2";
+import { useState } from "react";
+export default function PastShows({ results }) {
+  // console.log(results);
+  // console.log({ results });
+  const [state, setState] = useState({ search: "", searchResults: [] });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-export default function PastShows() {
+    setState((s) => ({ ...s, [name]: value }));
+  };
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const result = await fetch(`/api/search?searchDB=${state.search}`)
+      .then((j) => j.json())
+      .then((r) => r);
+    setState((s) => ({ ...s, searchResults: result }));
+  };
   return (
     <>
       <Layout>
@@ -22,7 +37,6 @@ export default function PastShows() {
         </Head>
         <article>
           <h1>Past Shows - 2021</h1>
-
           <h2>Earth in the Heavens </h2>
           <p>
             These works evocative of earthen material will be presented on the
@@ -39,22 +53,37 @@ export default function PastShows() {
             Nueman, Emily MacCloud, Bianca Boragi, Derek Des Islets, Bill
             Pierce, Mofaana Morojele, Robert Balun
           </p>
-
           <p> Inquire about specific shows prior to 2021 </p>
-
           <p>
             Find more on our instagram{" "}
             <a href="https://www.instagram.com/zxygallery/">
               <a>@zxygallery </a>
             </a>{" "}
           </p>
-          <Searchy2 />
-
           <h2>
             <Link href="/">Back to home</Link>
           </h2>
+
+          <input onChange={handleChange} name="search" />
+          <button type={"button"} onClick={handleSearch}>
+            Search
+          </button>
+          {state.searchResults.map((result) => (
+            <li>
+              <span>{result.artist} </span>
+              {result.medium1}
+            </li>
+          ))}
+          {/* <h2>Search our database for artists:</h2> */}
+          {/* const searchResults = []; */}
         </article>
       </Layout>
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const results = await getAllUsers();
+  const cleanResult = results.map((artist) => ({ ...artist, id: "abc" }));
+  return { props: { results: JSON.parse(JSON.stringify(cleanResult)) } };
+};
