ced298b3674c68110b539c20dc5de53fefaa93e2
Adam <chillcoders@gmail.com>
2023-01-31 18:04:38 -0500

    revert "search-prisma-jan30"
    This reverts commit 4d729639b9e5c7ca547b890f6b9b889faf56ea36.
    
    gh

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-### ZXY Gallery Official Open Source NextJS Website with Prisma and Cockroachdb
+ZXY Gallery Official Blog.
 
-## Make sure to also check out online.zxygallery.com
-
-## Looking for open source contributors!
-
-## contact us @zxygallery for more specific projects
-
-## if you see any improvements just fork the project and put in a pull request!
+Make sure to also check out online.zxygallery.com
--- pages/posts/pastshows.js
+++ pages/posts/pastshows.js
@@ -1,24 +1,9 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
-import { getAllUsers } from "../../components/Search2";
-import { useState } from "react";
-export default function PastShows({ results }) {
-  // console.log(results);
-  // console.log({ results });
-  const [state, setState] = useState({ search: "", searchResults: [] });
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+import Searchy2 from "../../components/Search2";
 
-    setState((s) => ({ ...s, [name]: value }));
-  };
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    const result = await fetch(`/api/search?searchDB=${state.search}`)
-      .then((j) => j.json())
-      .then((r) => r);
-    setState((s) => ({ ...s, searchResults: result }));
-  };
+export default function PastShows() {
   return (
     <>
       <Layout>
@@ -37,6 +22,7 @@ export default function PastShows({ results }) {
         </Head>
         <article>
           <h1>Past Shows - 2021</h1>
+
           <h2>Earth in the Heavens </h2>
           <p>
             These works evocative of earthen material will be presented on the
@@ -53,37 +39,22 @@ export default function PastShows({ results }) {
             Nueman, Emily MacCloud, Bianca Boragi, Derek Des Islets, Bill
             Pierce, Mofaana Morojele, Robert Balun
           </p>
+
           <p> Inquire about specific shows prior to 2021 </p>
+
           <p>
             Find more on our instagram{" "}
             <a href="https://www.instagram.com/zxygallery/">
               <a>@zxygallery </a>
             </a>{" "}
           </p>
+          <Searchy2 />
+
           <h2>
             <Link href="/">Back to home</Link>
           </h2>
-
-          <input onChange={handleChange} name="search" />
-          <button type={"button"} onClick={handleSearch}>
-            Search
-          </button>
-          {state.searchResults.map((result) => (
-            <li>
-              <span>{result.artist} </span>
-              {result.medium1}
-            </li>
-          ))}
-          {/* <h2>Search our database for artists:</h2> */}
-          {/* const searchResults = []; */}
         </article>
       </Layout>
     </>
   );
 }
-
-export const getServerSideProps = async () => {
-  const results = await getAllUsers();
-  const cleanResult = results.map((artist) => ({ ...artist, id: "abc" }));
-  return { props: { results: JSON.parse(JSON.stringify(cleanResult)) } };
-};
