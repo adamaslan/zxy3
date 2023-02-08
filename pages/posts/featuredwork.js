c19621a272e0c18a13ab009da481b76034180a35
Adam <chillcoders@gmail.com>
2023-02-08 14:24:44 -0500

    flex-style

--- pages/posts/featuredwork.js
+++ pages/posts/featuredwork.js
@@ -5,43 +5,44 @@ import Layout from "../../components/layout";
 import Image from "next/image";
 
 export default function FeaturedWork() {
-  return <>
-    <Layout>
-      <Head>
-        <title>Featured Work</title>
-        <link rel="icon" href="/public/favicon.ico" />
-        <meta
-          name="description"
-          content="Hear about this exquisite work for sale at ZXY"
-        />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/adamaslan/image/upload/v1666992137/ZXY%20/zxy-logo_cos9hl.jpg"
-        />
-      </Head>
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Featured Work</title>
+          <link rel="icon" href="/public/favicon.ico" />
+          <meta
+            name="description"
+            content="Hear about this exquisite work for sale at ZXY"
+          />
+          <meta
+            property="og:image"
+            content="https://res.cloudinary.com/adamaslan/image/upload/v1666992137/ZXY%20/zxy-logo_cos9hl.jpg"
+          />
+        </Head>
 
-      <h1>Featured Work</h1>
-      <h2>
-        <Image src="/chiara2.jpeg" alt="Flyer" width={810} height={1110} />
-        Abraxas by Chiara No
-        {/* We are pleased to offer this work at a discount price as part of our
+        <div className="flex-container">
+          <h2>
+            <Image src="/chiara2.jpeg" alt="Flyer" width={1000} height={1333} />
+            Abraxas by Chiara No
+            {/* We are pleased to offer this work at a discount price as part of our
         work of the month program aiming to connect collectors to artists. */}
-      </h2>
+          </h2>
 
-      {/* Image here */}
+          {/* Image here */}
 
-      <h3>
-        Mother of pearl drips from the tip of the phallus as a serpent clapper
-        hangs below the soundbow of the bell.
-        {/* Price reflects price of work. Price and details of shipping to be
+          <h3>
+            Mother of pearl drips from the tip of the phallus as a serpent
+            clapper hangs below the soundbow of the bell.
+            {/* Price reflects price of work. Price and details of shipping to be
         arranged with artist. */}
-        <br />
-        To hear the sound of the bell ringing click{" "}
-        <a href="https://vimeo.com/660146105">here</a>.
-      </h3>
-      <h2>Inquire for pricing and shipping details</h2>
+            <br /> <br />
+            To hear the sound of the bell ringing click{" "}
+            <a href="https://vimeo.com/660146105">here</a>.
+          </h3>
+          <h2>Inquire for pricing and shipping details</h2>
 
-      {/* <h1>Artists</h1>
+          {/* <h1>Artists</h1>
       <h2>We are very excited about these artists and curators:</h2>
       <p>Liz Ainslie </p>
 <p> Chiara No</p>
@@ -60,9 +61,9 @@ export default function FeaturedWork() {
       <p>Audra Lambert</p>
       <p>A. Timur Kilicaslan</p>
       <h2> */}
-      <Link href="/">
-        Back to home
-      </Link>
-    </Layout>
-  </>;
+          <Link href="/">Back to home</Link>
+        </div>
+      </Layout>
+    </>
+  );
 }
