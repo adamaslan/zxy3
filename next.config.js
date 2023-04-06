5a204ad3d536d2c7c333f8c69e33b9cf2997b383
Adam <chillcoders@gmail.com>
2023-04-06 02:32:20 -0400

    three

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+const withTM = require("next-transpile-modules")(["three"]);
+
+module.exports = withTM({
   images: {
     domains: ["res.cloudinary.com"],
   },
-
   reactStrictMode: true,
-};
+});
