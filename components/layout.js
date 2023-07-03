fa76d82e7d418ef052894339f2b3571ffa2ae1a7
Adam Aslan <adamaslan@Adams-MacBook-Air-2.local>
2023-07-03 08:18:53 -0700

    image-styling

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,4 @@
-import styles from "./layout.module.css";
+import styles from "../styles/layout.module.css";
 
 export default function Layout({ children }) {
   return <div className={styles.container}>{children}</div>;
